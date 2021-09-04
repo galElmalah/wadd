589f095e379b9390129de7bb50556e05ee565c8e
Gal Elmalah <gale@wix.com>
2021-09-04 14:45:09 +0300

    add install capabillity in lerna based monorepos

--- src/installer.ts
+++ src/installer.ts
@@ -4,20 +4,23 @@ import { exec, spawn } from 'child_process';
 const commandsCreatorsMap = {
   [Clients.yarn]:
     (packages: string[], isDev: boolean) => (workspaceName: string) =>
-      `yarn workspaces ${workspaceName} add ${packages.join(' ')} ${isDev ? '--dev' : ''
+      `yarn workspaces ${workspaceName} add ${packages.join(' ')}${
+        isDev ? ' --dev' : ''
       }`,
   [Clients.lerna]:
-    (packages: string[], isDev: boolean) => (workspaceName: string) =>
-      `yarn workspaces ${workspaceName} add ${packages.join(' ')} ${isDev ? '--dev' : ''
+    (isDev: boolean) => (packageToInstall: string, workspaceName: string) =>
+      `lerna add ${packageToInstall} --scope=${workspaceName}${
+        isDev ? ' --dev' : ''
       }`,
 };
 
-const execute = (at: string) => (command: string) => new Promise((resolve, reject) => {
-  const cp = spawn(command, [], { cwd: at, stdio: 'inherit' })
-  cp.on('close', resolve)
-  cp.on('exit', resolve)
-  cp.on('error', reject)
-})
+const execute = (at: string) => (command: string) =>
+  new Promise((resolve, reject) => {
+    const cp = spawn(command, [], { cwd: at, stdio: 'inherit' });
+    cp.on('close', resolve);
+    cp.on('exit', resolve);
+    cp.on('error', reject);
+  });
 
 export const installer = async (
   basePath: string,
@@ -31,6 +34,20 @@ export const installer = async (
       isDev
     );
 
-    await Promise.all(workspacesToInstallIn.map(commandCreator).map(execute(basePath)))
+    await Promise.all(
+      workspacesToInstallIn.map(commandCreator).map(execute(basePath))
+    );
+  }
+
+  if (client === Clients.lerna) {
+    const commands = [];
+    const commandCreator = commandsCreatorsMap[client](isDev);
+    packagesToInstall.forEach((packageToInstall) => {
+      workspacesToInstallIn.forEach((workspace) => {
+        commands.push(commandCreator(packageToInstall, workspace));
+      });
+    });
+
+    await Promise.all(commands.map(execute(basePath)));
   }
 };
