af3fa3ffb915f6e99efd0b604153bf0e6e582103
Gal Elmalah <gale@wix.com>
2021-09-04 14:25:09 +0300

    Find nearest monorepo functionallity

--- src/findPathToTheNearestMonorepo.ts
+++ src/findPathToTheNearestMonorepo.ts
@@ -1,7 +1,21 @@
-import {join} from 'path';
+import { join, parse } from 'path';
 import { getLernaJson, getRootPackageJson } from './readIfExists';
 
+export const findPathToTheNearestMonorepo = (fromPath: string) => {
+  const packageJson = getRootPackageJson(fromPath);
 
-const findPathToTheNearestMonorepo = (fromPath:string) => {
-  
-}
+  if (packageJson?.workspaces) {
+    return fromPath;
+  }
+
+  const lernaJson = getLernaJson(fromPath);
+  if (lernaJson?.packages) {
+    return fromPath;
+  }
+
+  if (fromPath === parse(fromPath).root) {
+    return;
+  }
+
+  return findPathToTheNearestMonorepo(join(fromPath, '..'));
+};
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,6 +2,7 @@ import { identifyClient } from '../src/identifyClient';
 import { getPackages } from '../src/getPackages';
 import { Clients } from '../src';
 import { aFixturePathGetter } from './fixtures';
+import {findPathToTheNearestMonorepo} from '../src/findPathToTheNearestMonorepo'
 
 describe('identifyClient', () => {
   const getFixturePath = aFixturePathGetter('identifyClient');
@@ -48,3 +49,41 @@ describe('getPackages', () => {
     expect(packageNames.sort()).toEqual(expectedPackages);
   });
 });
+
+
+describe.only('findPathToTheNearestMonorepo', () => {
+  const getFixturePath = aFixturePathGetter('findPathToTheNearestMonorepo');
+
+  it('should return nothing if no monorepo was detected', () => {
+    const basePath = getFixturePath('noMonorepo/a/b/c')
+    expect(findPathToTheNearestMonorepo(basePath)).toBeUndefined()
+
+  })
+
+  describe('Monorepo is detectable from package json', () => {
+    it('should get the path to monorepo when called with a nested path', () => {
+      const basePath = getFixturePath('hasMonorepo/inPackageJson/a/b/c')
+      expect(findPathToTheNearestMonorepo(basePath)).toBe(getFixturePath('hasMonorepo/inPackageJson'))
+    })
+
+    it('should get the path to monorepo when called with the actual path to the monorepo', () => {
+      const basePath = getFixturePath('hasMonorepo/inPackageJson')
+      expect(findPathToTheNearestMonorepo(basePath)).toBe(getFixturePath('hasMonorepo/inPackageJson'))
+    })
+  })
+
+  describe('Monorepo is detectable from lerna json', () => {
+    it('should get the path to monorepo when called with a nested path', () => {
+      const basePath = getFixturePath('hasMonorepo/inLernaJson/a/b/c')
+      expect(findPathToTheNearestMonorepo(basePath)).toBe(getFixturePath('hasMonorepo/inLernaJson'))
+    })
+
+    it('should get the path to monorepo when called with the actual path to the monorepo', () => {
+      const basePath = getFixturePath('hasMonorepo/inLernaJson')
+      expect(findPathToTheNearestMonorepo(basePath)).toBe(getFixturePath('hasMonorepo/inLernaJson'))
+    })
+  })
+
+
+
+})
